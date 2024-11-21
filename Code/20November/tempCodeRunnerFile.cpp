b38fc1f59a7ddd597e5a9a492e20723ecf183b6d
Shiva <shivabhaggan@gmail.com>
2024-11-20 19:22:48 -0500

    excercise

--- Code/20November/tempCodeRunnerFile.cpp
+++ Code/20November/tempCodeRunnerFile.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main() {
-    std::cout << "Hello, students!" << std::endl;
-    int age;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-    std::cout << "You entered: " << age << std::endl;
+    // std::cout << "Hello, students!" << std::endl;
+    
+    // std::cout << "Enter your age: ";
+    // std::cin >> age;
+    // std::cout << "You entered: " << age << std::endl;
+
     string name;
+    int age;
     cout << "Enter your name: ";
     cin >> name;
     cout << "Hello, " << name << "! You are " << age << " years old." << endl;
